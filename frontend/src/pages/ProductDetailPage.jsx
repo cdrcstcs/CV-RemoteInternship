d21ba9cd8a6afb2296ed961af5a1d9dfa6a003aa
ndhieu <ndhieu@ndhieus-MBP.lan>
2025-02-17 18:22:23 +0800

    save and gonna fix all bugs

--- frontend/src/pages/ProductDetailPage.jsx
+++ frontend/src/pages/ProductDetailPage.jsx
@@ -40,7 +40,7 @@ const ProductDetailPage = () => {
   }, [products]);
 
   const handleAddToCart = () => {
-    addToCart(currentProduct);
+    addToCart(currentProduct.id);
   };
 
   const handleFeedbackSubmit = async (e) => {
